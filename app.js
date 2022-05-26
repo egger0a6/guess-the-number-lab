e2e6fef0e7b40fb4a0261232e678b0c734fe8793
Zach Eggert <egger0a6@email.arizona.edu>
2022-05-26 17:09:51 -0400

    add getGuess() and render() obj functions. Add skeleton for updateRange() obj function that updates biggest or smallest number during the game loop.

--- app.js
+++ app.js
@@ -3,8 +3,45 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  totGuesses: 0,
+  getGuess: function() {
+    let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
+
+    while(true) {
+      guess = parseInt(guess);
+      if(guess !== NaN && guess >= this.smallestNum && guess <= this.biggestNum) {
+        break;
+      }
+      else {
+        guess = prompt(`Please enter a valid number between ${this.smallestNum} and ${this.biggestNum}`)
+      }
+    }
+
+    return guess;
+  }, 
+  render: function(guess) {
+    if (guess === secretNum) {
+      alert(`Congrats! You guessed the number in ${this.totGuesses} guesses!`)
+    }
+    else {
+      alert(`Sorry. Your guess is too ${(guess >= this.secretNum) ? "high" : "low"}. Previous guesses: ${this.prevGuesses.join(", ").trim()}.`)
+    }
+  },
+  updateRange: function() {
+
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+
+    let guess = this.getGuess();
+    this.prevGuesses.push(guess);
+    this.totGuesses++;
+
+    if(this.render(guess));
+  },
 }
+
+game.play();
+console.log(game.prevGuesses);
